f1ffde29d5d7ac2706f8c5bb293dc6b8d3d1dcf7
Michael Keymolen <michael.keymolen@kenze.be>
2018-08-14 14:42:35 +0200

    Possible to use scopeprefix together with scopes that have another prefix

--- src/Hexiron.Azure.ActiveDirectory/Models/AzureAdB2COptions.cs
+++ src/Hexiron.Azure.ActiveDirectory/Models/AzureAdB2COptions.cs
@@ -1,4 +1,7 @@
-﻿namespace Hexiron.Azure.ActiveDirectory.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Hexiron.Azure.ActiveDirectory.Models
 {
     public class AzureAdB2COptions
     {
@@ -25,8 +28,40 @@
         public string Authority => $"{Domain}/{SignUpSignInPolicyId}/v2.0";
         public string Domain => $"{_azureAdB2CInstance}/{Tenant}";
 
+        public string ScopePrefix { get; set; }
+
+        public string[] Scopes { get; set; }
         // a space seperated list of necessary scopes for accessing the api 
-        public string[] ApiScopes { get; set; }
+        public string[] ApiScopes
+        {
+            get
+            {   
+                var scopeList = new List<string>();
+                if (Scopes != null)
+                {
+                    if (!string.IsNullOrEmpty(ScopePrefix))
+                    {
+                        foreach (var scope in Scopes)
+                        {
+                            if (!scope.ToLower().StartsWith("http"))
+                            {
+                                scopeList.Add(scope);
+                            }
+                            else
+                            {
+                                scopeList.Add($"{ScopePrefix}/{scope}");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        return Scopes;
+                    }
+                }
+                
+                return scopeList.ToArray();
+            }
+        }
 
     }
 }
