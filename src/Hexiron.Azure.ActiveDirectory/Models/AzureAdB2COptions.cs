51dfef625282495704a97527a006c60bbb64d869
Michael Keymolen <michael.keymolen@kenze.be>
2018-09-03 11:13:50 +0200

    Added support for other custom defined policies

--- src/Hexiron.Azure.ActiveDirectory/Models/AzureAdB2COptions.cs
+++ src/Hexiron.Azure.ActiveDirectory/Models/AzureAdB2COptions.cs
@@ -5,7 +5,7 @@ namespace Hexiron.Azure.ActiveDirectory.Models
 {
     public class AzureAdB2COptions
     {
-        //public const string POLICY_AUTHENTICATION_PROPERTY = "Policy";
+        public const string POLICY_AUTHENTICATION_PROPERTY = "Policy";
         private readonly string _azureAdB2CInstance;
 
         public AzureAdB2COptions()
@@ -16,16 +16,10 @@ namespace Hexiron.Azure.ActiveDirectory.Models
 
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
-
         public string Tenant { get; set; }
-        public string SignUpSignInPolicyId { get; set; }
-        //public string SignInPolicyId { get; set; }
-        //public string SignUpPolicyId { get; set; }basix
-        //public string ResetPasswordPolicyId { get; set; }
-        //public string EditProfilePolicyId { get; set; }
-
+        public string DefaultPolicy { get; set; }
         public string RedirectUri { get; set; }
-        public string Authority => $"{Domain}/{SignUpSignInPolicyId}/v2.0";
+        public string Authority => $"{Domain}/{DefaultPolicy}/v2.0";
         public string Domain => $"{_azureAdB2CInstance}/{Tenant}";
 
         public string ScopePrefix { get; set; }
